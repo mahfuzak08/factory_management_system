559f3ba27093e3ae9be93d5b274e1235c8c6d4a0
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2023-09-17 17:11:29 +0600

    employee done

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-// use App\Models\AccountTranx;
-// use App\Models\Bankacc;
+use App\Models\AccountTranx;
+use App\Models\Bankacc;
 use App\Models\Employee;
 
 class EmployeeController extends Controller
@@ -114,4 +114,69 @@ class EmployeeController extends Controller
         flash()->addSuccess('Data Delete Successfully.');
         return redirect('employee');
     }
+
+    public function see_employee($id){
+        $employee = Employee::findOrFail($id);
+        $banks = Bankacc::where('type', '!=', 'Due')->get();
+        if(! empty(request()->input('search'))){
+            $str = request()->input('search');
+            $datas = AccountTranx::join('bankaccs', 'account_tranxes.account_id', '=', 'bankaccs.id')
+                            ->where(function ($query) use ($str){
+                                $query->where('tranx_date', 'like', '%'.$str.'%')
+                                ->orWhere('amount', 'like', '%'.$str.'%')
+                                ->orWhere('bankaccs.name', 'like', '%'.$str.'%')
+                                ->orWhere('note', 'like', '%'.$str.'%');
+                            })
+                            ->where('ref_id', $id)
+                            ->where('ref_type', 'employee')
+                            ->select('account_tranxes.*', 'bankaccs.name as bank_name')
+                            ->latest()->paginate(10);
+        }else{
+            $datas = AccountTranx::join('bankaccs', 'account_tranxes.account_id', '=', 'bankaccs.id')
+                    ->where('ref_id', $id)
+                    ->where('ref_type', 'employee')
+                    ->select('account_tranxes.*', 'bankaccs.name as bank_name')
+                    ->latest()->paginate(10);
+        }
+        $total_receive = AccountTranx::where('ref_id', $id)->where('ref_type', 'employee')->sum('amount');
+        return view('admin.employee.details', compact('employee', 'banks', 'datas', 'total_receive'))->with('i', (request()->input('page', 1) - 1) * 10);
+    }
+
+    public function add_amount(Request $request){
+        $rules = [
+            'account_id' => ['required'],
+            'tranx_date' => ['required', 'date'],
+            'amount' => ['required', 'numeric']
+        ];
+        $id = $request->input('account_id');
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            foreach ($validator->messages()->toArray() as $key => $value) { 
+                flash()->addError($value[0]);
+            }
+            return redirect($request->input('redirect_url'));
+        }
+
+        DB::beginTransaction();
+        try{
+            $data = new AccountTranx();
+            
+            $input = $request->all();
+            $input['amount'] *= -1;
+            $input['user_id'] = Auth::id();
+            
+            $data->fill($input)->save();
+    
+            flash()->addSuccess('New Data Added Successfully.');
+            // If all queries succeed, commit the transaction
+            DB::commit();
+        }catch (\Exception $e) {
+            // If any query fails, catch the exception and roll back the transaction
+            flash()->addError('Data Not Added Successfully.');
+            DB::rollback();
+        }
+        return redirect($request->input('redirect_url'));
+    }
+
 }
---
+++ resources/views/admin/employee/details.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin._head')
+  </head>
+  <body>
+    <div class="container-scroller">
+      @include('admin._navbar')
+      <div class="container-fluid page-body-wrapper">
+        @include('admin._sidebar')
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="page-header">
+                  <h3 class="page-title">{{__('admin.employee')}}</h3>
+                  <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                      <li class="breadcrumb-item"><a href="{{route('employee')}}" class="btn btn-sm btn-rounded btn-secondary">{{__('admin.back')}}</a></li>
+                    </ol>
+                  </nav>
+                </div>
+                <div class="row">
+                    <div class="col-12 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <div class="forms-sample">
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input1" class="col-sm-3 col-form-label">{{__('admin.name')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" class="form-control form-control-border-off" disabled="true" id="input1" value="{{$employee->name}}">
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input2" class="col-sm-3 col-form-label">{{__('admin.mobile')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="email" class="form-control form-control-border-off" disabled="true" id="input2" value="{{$employee->mobile}}">
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input3" class="col-sm-3 col-form-label">{{__('admin.address')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" class="form-control form-control-border-off" disabled="true" id="input3" value="{{$employee->address}}">
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input3" class="col-sm-3 col-form-label">{{__('admin.salary')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" class="form-control form-control-border-off" disabled="true" id="input3" value="{{$employee->salary}}">
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input3" class="col-sm-3 col-form-label">{{__('admin.receive_amount')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" class="form-control form-control-border-off" disabled="true" id="input3" value="{{abs($total_receive)}}">
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col-md-6 d-none d-md-block" id="addForm">
+                                  <form class="forms-sample" method="POST" action="{{ route('save-employee-amount') }}">
+                                    @csrf
+                                    <input type="hidden" name="ref_id" value="{{$employee->id}}" />
+                                    <input type="hidden" name="ref_type" value="employee" />
+                                    <input type="hidden" name="redirect_url" value="employee_details/{{$employee->id}}" />
+                                    <input type="hidden" name="type" value="withdrawal" />
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input6" class="col-sm-3 col-form-label">{{__('admin.date')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" name="tranx_date" class="form-control" id="input6" value="{{date('Y-m-d')}}">
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input8" class="col-sm-3 col-form-label">{{__('admin.enter_your_amount')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" name="amount" placeholder="{{__('admin.enter_your_amount')}}" required class="form-control" id="input8">
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input9" class="col-sm-3 col-form-label">{{__('admin.account_name')}}</label>
+                                      <div class="col-sm-9">
+                                        <select class="form-select" name="account_id" id="input9" aria-label="Default select example">
+                                          @foreach($banks as $bank)
+                                          <option value="{{$bank->id}}">{{$bank->name}}</option>
+                                          @endforeach
+                                        </select>
+                                      </div>
+                                    </div>
+                                    <div class="form-group form-group-margin-bottom-off row">
+                                      <label for="input7" class="col-sm-3 col-form-label">{{__('admin.details')}}</label>
+                                      <div class="col-sm-9">
+                                        <input type="text" class="form-control" name="note" id="input7" placeholder="{{__('admin.details')}}">
+                                      </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary me-2 float-end">{{ __('admin.save_now') }}</button>
+                                  </form>
+                                </div>
+                                <div class="col-md-6 d-block d-md-none text-center">
+                                  <br />
+                                  <a onclick="openForm()" class="btn btn-sm btn-rounded btn-info">{{__('admin.add_new')}}</a>
+                                </div>
+                                <div class="col-12">
+                                  <br />
+                                  <hr />
+                                  <br />
+                                  <form action="{{route('employee-details', $employee->id)}}" method="GET">
+                                    @csrf
+                                    @php 
+                                    $sv = isset($_GET['search']) ? $_GET['search'] : '';
+                                    @endphp
+                                    <div class="row">
+                                      <input type="text" name="search" class="col-12 col-md-10" value="{{$sv}}" placeholder="{{__('admin.what_you_want_to_find')}}">
+                                      <button type="submit" class="col-12 col-md-2 btn btn-info">{{__('admin.find')}}</button>
+                                    </div>
+                                  </form>
+                                </div>
+                              </div>
+                              <br />
+                              <hr />
+                              <div class="row table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>{{__('admin.sl')}}</th>
+                                            <th>{{__('admin.date')}}</th>
+                                            <th>{{__('admin.account_name')}}</th>
+                                            <th>{{__('admin.details')}}</th>
+                                            <th class="text-right">{{__('admin.enter_your_amount')}}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                      @if(count($datas) > 0)
+                                        @php 
+                                        if(isset($_GET['page']) && $_GET['page']>0)
+                                          $n = 1 + (($_GET['page'] - 1) * 10);
+                                        else
+                                          $n = 1;
+                                        @endphp
+                                        @foreach($datas as $row)
+                                          <tr>
+                                            <td>{{$n++}}</td>
+                                            <td>{{$row->tranx_date}}</td>
+                                            <td>{{$row->bank_name}}</td>
+                                            <td>{{$row->note}}</td>
+                                            <td class="text-right">{{number_format($row->amount, 2)}}</td>
+                                          </tr>
+                                        @endforeach
+                                      @else
+                                          <tr>
+                                            <td colspan="5" class="text-center">{{__('admin.no_data_found')}}</td>
+                                          </tr>
+                                      @endif
+                                    </tbody>
+                                </table>
+                              </div>
+                              {{ $datas->onEachSide(3)->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          @include('admin._footer')
+        </div>
+      </div>
+    </div>
+    @include('admin._script')
+    <script>
+      function openForm(){
+        $('#addForm').removeClass('d-none');
+      }
+    </script>
+  </body>
+</html>
