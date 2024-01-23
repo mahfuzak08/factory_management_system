9214df374057dedcc1234e72b5e270b06562fea0
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2024-01-23 17:23:36 +0600

    up

--- resources/views/admin/report/profitnloss.blade.php
+++ resources/views/admin/report/profitnloss.blade.php
@@ -110,14 +110,14 @@
                                     <td style="text-align: right; width: 200px;padding: 5px 10px;">{{number_format($total['sales'] - $total['receive'], 2)}}</td>
                                   </tr>
                                   <tr style="border: 1px solid #000;">
-                                    <td style="padding: 5px 10px;">{{__('admin.accounts_payable')}}</td>
-                                    <td style="text-align: right; width: 200px;"></td>
-                                    <td style="text-align: right; width: 200px;padding: 5px 10px;">{{number_format($total['purchase'] - $total['pay'], 2)}}</td>
-                                  </tr>
-                                  <tr style="border: 1px solid #000;">
                                     <td style="padding: 5px 10px;">{{__('admin.total_payment')}}</td>
                                     <td style="text-align: right; width: 200px;"></td>
                                     <td style="text-align: right; width: 200px;">{{number_format($total['receive'], 2)}}</td>
+                                  </tr>
+                                  <tr style="border: 1px solid #000;">
+                                    <td style="padding: 5px 10px;">{{__('admin.accounts_payable')}}</td>
+                                    <td style="text-align: right; width: 200px;"></td>
+                                    <td style="text-align: right; width: 200px;padding: 5px 10px;">{{number_format($total['purchase'] - $total['pay'], 2)}}</td>
                                   </tr>
                                   <tr style="border: 1px solid #000;">
                                     <td style="padding: 5px 10px;">{{__('admin.quantity')}}</td>
