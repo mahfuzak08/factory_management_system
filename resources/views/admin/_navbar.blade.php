6b2c266552fd757cadfa39b4d8ad8764b7fa557c
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2024-04-03 16:53:43 +0600

    some design update

--- resources/views/admin/_navbar.blade.php
+++ resources/views/admin/_navbar.blade.php
@@ -1,23 +1,19 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-    <a class="navbar-brand brand-logo" href="{{ route('dashboard') }}">{{config('app.name')}}</a>
-    <a class="navbar-brand brand-logo-mini" style="width: 30px; margin-left: 10px;" href="{{ route('dashboard') }}">{{config('app.name')}}</a>
-    {{-- <a class="navbar-brand brand-logo" href="index.html"><img src="admin/assets/images/logo.svg" alt="logo" /></a>
-    <a class="navbar-brand brand-logo-mini" href="index.html"><img src="admin/assets/images/logo-mini.svg" alt="logo" /></a> --}}
+    <a class="navbar-brand brand-logo" style="margin-left: 30px;float: left;" href="{{ route('dashboard') }}">
+      <img src="pollifeed-logo-mini.png" style="width: 36px;height: 36px;float: left;top: 15px;position: absolute;" alt="{{config('app.name')}} logo" /> 
+      <div style="font-size: 20px;font-weight: bold;">{{config('app.name')}} </div>
+    </a>
+    <a class="navbar-brand brand-logo-mini" style="margin-left: 15px;float: left;" href="{{ route('dashboard') }}">
+      <img src="pollifeed-logo-mini.png" style="width: 36px;height: 36px;float: left;top: 15px;position: absolute;" alt="{{config('app.name')}} logo" /> 
+      <div class="logo-mini-title" style="font-size: 24px;position: relative;margin: 100%;">{{config('app.name')}} </div>
+    </a>
   </div>
   <div class="navbar-menu-wrapper d-flex align-items-stretch">
     <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
       <span class="mdi mdi-menu"></span>
     </button>
     <div class="search-field d-none d-md-block">
-      <form class="d-flex align-items-center h-100" action="#">
-        <div class="input-group">
-          <div class="input-group-prepend bg-transparent">
-            <i class="input-group-text border-0 mdi mdi-magnify"></i>
-          </div>
-          <input type="text" class="form-control bg-transparent border-0" placeholder="Search projects">
-        </div>
-      </form>
     </div>
     <ul class="navbar-nav navbar-nav-right"></ul>
     <x-app-layout>
