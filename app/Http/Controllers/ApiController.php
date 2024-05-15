b7c8c0c91a2c365e4fdbbae6c5e31acc2804e9b8
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2024-05-15 14:43:18 +0600

    up

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,47 +13,47 @@ class ApiController extends Controller
     public function receiveData(Request $request)
     {
         $device_attendance = $request->all();
-        try{
-            DB::beginTransaction();
-            if(count($device_attendance)>0){
-                $ddata = array();
-                $flag = array();
-                $eattdata = array();
-                foreach($device_attendance as $row){
-                    $ddata[] = array(
-                        'uid'=>$row['uid'],
-                        'emp_id'=>$row['user_id'],
-                        'state'=>$row['status'],
-                        'timestamp'=>$row['timestamp'],
-                        'type'=>$row['type']
-                    );
-                    // if($row['user_id'] != '902770' && array_search($row['user_id'], $flag, true) === false){
-                    //     $empatt = Attendance::where('emp_id', $row['user_id'])->where('date', date('Y-m-d', strtotime($row['timestamp'])))->pluck('id');
-                    //     if(count($empatt) == 0){
-                    //         $flag[] = $row["user_id"];
-                    //         $eattdata[] = array(
-                    //             'date'=>date('Y-m-d', strtotime($row['timestamp'])),
-                    //             'emp_id'=>$row['user_id'],
-                    //             'hours'=>8,
-                    //             'user_id'=>1,
-                    //             'created_at'=>date('Y-m-d H:i:s', time()),
-                    //             'updated_at'=>date('Y-m-d H:i:s', time()),
-                    //         );
-                    //     }
-                    // }
-                }
-                if(count($ddata)>0)
-                    Device_attendance::insert($ddata);
-                // if(count($eattdata)>0)
-                //     Attendance::insert($eattdata);
+        // try{
+        //     DB::beginTransaction();
+        //     if(count($device_attendance)>0){
+        //         $ddata = array();
+        //         $flag = array();
+        //         $eattdata = array();
+        //         foreach($device_attendance as $row){
+        //             $ddata[] = array(
+        //                 'uid'=>$row['uid'],
+        //                 'emp_id'=>$row['user_id'],
+        //                 'state'=>$row['status'],
+        //                 'timestamp'=>$row['timestamp'],
+        //                 'type'=>$row['type']
+        //             );
+        //             // if($row['user_id'] != '902770' && array_search($row['user_id'], $flag, true) === false){
+        //             //     $empatt = Attendance::where('emp_id', $row['user_id'])->where('date', date('Y-m-d', strtotime($row['timestamp'])))->pluck('id');
+        //             //     if(count($empatt) == 0){
+        //             //         $flag[] = $row["user_id"];
+        //             //         $eattdata[] = array(
+        //             //             'date'=>date('Y-m-d', strtotime($row['timestamp'])),
+        //             //             'emp_id'=>$row['user_id'],
+        //             //             'hours'=>8,
+        //             //             'user_id'=>1,
+        //             //             'created_at'=>date('Y-m-d H:i:s', time()),
+        //             //             'updated_at'=>date('Y-m-d H:i:s', time()),
+        //             //         );
+        //             //     }
+        //             // }
+        //         }
+        //         if(count($ddata)>0)
+        //             Device_attendance::insert($ddata);
+        //         // if(count($eattdata)>0)
+        //         //     Attendance::insert($eattdata);
                 
-                DB::commit();
-            }
-        }catch(\Exception $e) {
-            DB::rollback();
-            // activity()->log('Failed to save device data. Bcoz' . $e->getMessage());
-            return response()->json(array("status"=> false, "error1"=>$e->getMessage()));
-        }
+        //         DB::commit();
+        //     }
+        // }catch(\Exception $e) {
+        //     DB::rollback();
+        //     // activity()->log('Failed to save device data. Bcoz' . $e->getMessage());
+        //     return response()->json(array("status"=> false, "error1"=>$e->getMessage()));
+        // }
         // activity()->log('Device data save successfully at ' . date('Y-m-d h:i:s a', time()));
         return response()->json(["status"=> true, "message"=> "Device data save successfully", "device_attendance"=>$device_attendance ]);
     }
