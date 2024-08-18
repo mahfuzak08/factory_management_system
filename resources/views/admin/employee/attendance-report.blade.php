ea6f528bb211a8fad40bdc1e2679800b2c11808f
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2024-08-18 16:30:31 +0600

    report design update

--- resources/views/admin/employee/attendance-report.blade.php
+++ resources/views/admin/employee/attendance-report.blade.php
@@ -2,6 +2,25 @@
 <html lang="en">
   <head>
     @include('admin._head')
+    <style>
+      .nopad8px{
+        padding: 0px 5px !important;
+        font-size: 12px !important;
+      }
+      .table-responsive {
+        max-height: 500px; /* Adjust this value as needed */
+        overflow-y: auto;
+        position: relative;
+      }
+      .table-responsive thead th {
+        position: sticky;
+        top: 0;
+        background-color: #86888b; /* Same as .thead-dark background */
+        color: white; /* Same as .thead-dark text color */
+        z-index: 1;
+        box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+      }
+    </style>
   </head>
   <body>
     <div class="container-scroller">
@@ -9,10 +28,10 @@
       <div class="container-fluid page-body-wrapper">
         @include('admin._sidebar')
         <div class="main-panel">
-            <div class="content-wrapper" style="max-height: 100px">
-              <form id="dataForm" action="{{route('emp-report')}}" method="GET">
-                @csrf
-                <div class="page-header row">
+            <div class="content-wrapper">
+                <form id="dataForm" action="{{route('emp-report')}}" method="GET">
+                  @csrf
+                  <div class="page-header row">
                     <div class="col-4">
                       <h3 class="page-title">{{__('admin.employee')}}</h3>
                     </div>
@@ -28,19 +47,6 @@
                     <div class="col-3"><button class="btn btn-info" type="submit">{{__('admin.old_attendance')}}</button></div>
                   </div>
                 </form>
-                  {{-- <nav> --}}
-                    {{-- <ol> --}}
-                        {{-- <li> --}}
-                          {{-- <form id="dataForm" action="{{route('emp-report')}}" method="GET">
-                            @csrf
-                            <input type="month" name="month" class="form-control">
-                            <button class="btn btn-info" type="submit">{{__('admin.old_attendance')}}</button>
-                          </form> --}}
-                          {{-- <button id="printButton" class="btn btn-success hidden">Print</button> --}}
-                        {{-- </li> --}}
-                    {{-- </ol> --}}
-                  {{-- </nav> --}}
-                </div>
                 <div class="row">
                     <div class="col-12 grid-margin stretch-card">
                         <div class="card">
@@ -58,7 +64,7 @@
                                             <th>#</th>
                                             <th> {{__('admin.name')}} </th>
                                             @for($i=1; $i<=$totalDays; $i++)
-                                                <th>{{$i}}</th>
+                                                <th class="nopad8px">{{$i}}</th>
                                             @endfor
                                             <th>Total</th>
                                           </tr>
@@ -90,12 +96,12 @@
                                                     @php
                                                     $total+= $employee[$n]['attendance'][$j]['hours']>=8 ? 1 : ($employee[$n]['attendance'][$j]['hours'] >= 4 ? 0.5 : 0);
                                                     @endphp
-                                                    <td>{{$employee[$n]['attendance'][$j]['hours']>=8 ? 'Y' : ($employee[$n]['attendance'][$j]['hours'] >= 4 ? 'H' : '')}}</td>
+                                                    <td class="nopad8px">{{$employee[$n]['attendance'][$j]['hours']>=8 ? 'Y' : ($employee[$n]['attendance'][$j]['hours'] >= 4 ? 'H' : '')}}</td>
                                                 @else
-                                                    <td> </td>
+                                                    <td class="nopad8px"> </td>
                                                 @endif
                                             @endfor
-                                            <td>
+                                            <td class="nopad8px">
                                               {{$total}}
                                             </td>
                                         </tr>
