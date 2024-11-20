a61236c7f7faba1c120c95e447cfe681e14bf163
Md. Mahfuzur Rahman <mahfuzak08@gmail.com>
2024-11-21 01:24:41 +0600

    up

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -93,14 +93,17 @@ class ReportController extends Controller
         $banks = Bankacc::get();
         $accounts_payable_bid = 0;
         $accounts_receivable_bid = 0;
+        $cash_bid = 0;
         foreach($banks as $r){
             if($r->type == 'Due' && $r->name == 'Due')
                 $accounts_payable_bid = $r->id;
             if($r->type == 'Due' && $r->name == 'Due2')
                 $accounts_receivable_bid = $r->id;
+            if($r->type == 'Cash' && $r->name == 'Cash')
+                $cash_bid = $r->id;
         }
         
-        $data['accounts_payable'] = AccountTranx::where("ref_type","=", "customer")->sum('amount') - AccountTranx::where("ref_type","=", "customer")->where('account_id', '=', $accounts_payable_bid)->sum('amount');
+        $data['accounts_payable'] = AccountTranx::where("ref_type","=", "customer")->where('account_id', '=', $cash_bid)->sum('amount') - AccountTranx::where("ref_type","=", "customer")->where('account_id', '=', $accounts_payable_bid)->sum('amount');
         $data['accounts_receivable'] = AccountTranx::where('account_id', '=', $accounts_receivable_bid)->sum('amount');
         return view('admin.report.due', compact('data'));
     }
